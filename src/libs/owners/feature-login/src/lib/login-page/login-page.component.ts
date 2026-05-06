1ca5ab4de92e32141ec33409eeb172c346c070b3
Fidel Zarco <fidel.zarco1608@gmail.com>
2026-05-05 22:32:09 -0600

    Agregué i18n

--- src/libs/owners/feature-login/src/lib/login-page/login-page.component.ts
+++ src/libs/owners/feature-login/src/lib/login-page/login-page.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { LoginFormComponent } from "../../../../ui/src";
 import { LoginOwnerDto, OwnerStore } from "../../../../data-access/src";
+import { TranslatePipe } from "@ngx-translate/core";
 
 
 @Component({
     selector:'app-owner-login-page',
     standalone:true,
-    imports: [CommonModule, LoginFormComponent],
+    imports: [CommonModule, LoginFormComponent, TranslatePipe],
     templateUrl:'./login-page.component.html'
 })
 export class LoginPageComponent{
--- src/libs/owners/ui/src/lib/login-form/login-form.component.ts
+++ src/libs/owners/ui/src/lib/login-form/login-form.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from "@angular/common";
 import { Component, EventEmitter, inject, Input, Output } from "@angular/core";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { email } from "@angular/forms/signals";
+import { TranslatePipe } from "@ngx-translate/core";
 
 
 @Component({
     selector: 'app-owner-login-form',
     standalone:true,
-    imports:[CommonModule, ReactiveFormsModule],
+    imports:[CommonModule, ReactiveFormsModule, TranslatePipe],
     templateUrl: './login-form.component.html',
 })
 export class LoginFormComponent{
