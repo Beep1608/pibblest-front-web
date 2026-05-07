9bc5fbc90ffccca109dbd93b8659a04d6e1b0fc2
BeepBoop <fidel.zarcoa@sfa.michoacan.gob.mx>
2026-05-07 16:31:18 -0600

    Agregué libreria de componentes y ui para ver las tiendas, falta consumir el backend

---
+++ src/libs/stores/data-access/lib/models/store.model.ts
@@ -0,0 +1,10 @@
+
+export interface StorePreview{
+    name: string,
+    growthFromStart: number,
+    employees: number,
+    salesOfToday: number,
+    createdAt: string,
+    totalProducts: number,
+    currentQuantityOfProducts: number
+}
--- src/libs/stores/feature-view-all/src/lib/viewAll/viewAll.component.ts
+++ src/libs/stores/feature-view-all/src/lib/viewAll/viewAll.component.ts
@@ -1,14 +1,51 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { TranslatePipe } from "@ngx-translate/core";
+import { StorePreview } from "../../../../data-access/lib/models/store.model";
 
 
 @Component({
-    selector:'app-store-view-all-page',
-    imports:[CommonModule, TranslatePipe],
-    templateUrl:'./viewAll.component.html',
-
+  selector: 'app-store-view-all-page',
+  imports: [CommonModule, TranslatePipe],
+  templateUrl: './viewAll.component.html',
 })
-export class StoreViewAllPage{
-    
+export class StoreViewAllPage {
+  stores = signal<StorePreview[]>([
+    {
+      name: 'Abarrote',
+      growthFromStart: 80.2,
+      employees: 2,
+      salesOfToday: 26,
+      createdAt: '01/02/2026',
+      totalProducts: 100,
+      currentQuantityOfProducts: 50,
+    },
+    {
+      name: 'Abarrote',
+      growthFromStart: 80.2,
+      employees: 2,
+      salesOfToday: 26,
+      createdAt: '01/02/2026',
+      totalProducts: 100,
+      currentQuantityOfProducts: 50,
+    },
+    {
+      name: 'Abarrote',
+      growthFromStart: 80.2,
+      employees: 2,
+      salesOfToday: 26,
+      createdAt: '01/02/2026',
+      totalProducts: 100,
+      currentQuantityOfProducts: 50,
+    },
+    {
+      name: 'Abarrote',
+      growthFromStart: 80.2,
+      employees: 2,
+      salesOfToday: 26,
+      createdAt: '01/02/2026',
+      totalProducts: 100,
+      currentQuantityOfProducts: 50,
+    },
+  ]);
 }
