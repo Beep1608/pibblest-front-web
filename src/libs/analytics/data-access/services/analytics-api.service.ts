a8be48fb565e06ff6b5afd032b0b3a18e65fb304
BeepBoop <fidel.zarcoa@sfa.michoacan.gob.mx>
2026-06-19 08:43:04 -0600

    Ruta cambiada en analytics

--- src/libs/analytics/data-access/services/analytics-api.service.ts
+++ src/libs/analytics/data-access/services/analytics-api.service.ts
@@ -5,37 +5,43 @@ import { AnalyticsFilters, CurrencySeriesResponse, RankedDataResponse, ProductRa
 
 @Injectable({ providedIn: 'root' })
 export class AnalyticsApiService {
-  private baseUrl = '/api/analytics';
+	private baseUrl = 'http://localhost:8081/api/analytics';
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {}
 
-  private createParams(filters: AnalyticsFilters): HttpParams {
-    let params = new HttpParams()
-      .set('range', filters.range)
-      .set('granularity', filters.granularity);
-    if (filters.startDate) params = params.set('startDate', filters.startDate);
-    if (filters.endDate) params = params.set('endDate', filters.endDate);
-    if (filters.storeId) params = params.set('storeId', filters.storeId.toString());
-    return params;
-  }
+	private createParams(filters: AnalyticsFilters): HttpParams {
+		let params = new HttpParams().set('range', filters.range).set('granularity', filters.granularity);
+		if (filters.startDate) params = params.set('startDate', filters.startDate);
+		if (filters.endDate) params = params.set('endDate', filters.endDate);
+		if (filters.storeId) params = params.set('storeId', filters.storeId.toString());
+		return params;
+	}
 
-  getRevenueSeries(filters: AnalyticsFilters): Observable<CurrencySeriesResponse> {
-    return this.http.get<CurrencySeriesResponse>(`${this.baseUrl}/sales-volume`, { params: this.createParams(filters) });
-  }
+	getRevenueSeries(filters: AnalyticsFilters): Observable<CurrencySeriesResponse> {
+		return this.http.get<CurrencySeriesResponse>(`${this.baseUrl}/sales-volume`, {
+			params: this.createParams(filters),
+		});
+	}
 
-  getSalesCountSeries(filters: AnalyticsFilters): Observable<CurrencySeriesResponse> {
-    return this.http.get<CurrencySeriesResponse>(`${this.baseUrl}/sales-count`, { params: this.createParams(filters) });
-  }
+	getSalesCountSeries(filters: AnalyticsFilters): Observable<CurrencySeriesResponse> {
+		return this.http.get<CurrencySeriesResponse>(`${this.baseUrl}/sales-count`, { params: this.createParams(filters) });
+	}
 
-  getBestSellingProducts(filters: AnalyticsFilters): Observable<RankedDataResponse<ProductRankEntry>> {
-    return this.http.get<RankedDataResponse<ProductRankEntry>>(`${this.baseUrl}/best-selling-products`, { params: this.createParams(filters) });
-  }
+	getBestSellingProducts(filters: AnalyticsFilters): Observable<RankedDataResponse<ProductRankEntry>> {
+		return this.http.get<RankedDataResponse<ProductRankEntry>>(`${this.baseUrl}/best-selling-products`, {
+			params: this.createParams(filters),
+		});
+	}
 
-  getPeakHours(filters: AnalyticsFilters): Observable<RankedDataResponse<PeakHourEntry>> {
-    return this.http.get<RankedDataResponse<PeakHourEntry>>(`${this.baseUrl}/peak-hours`, { params: this.createParams(filters) });
-  }
-  
-  getStorePerformance(filters: AnalyticsFilters): Observable<RankedDataResponse<StoreRankEntry>> {
-    return this.http.get<RankedDataResponse<StoreRankEntry>>(`${this.baseUrl}/top-stores`, { params: this.createParams(filters) });
-  }
+	getPeakHours(filters: AnalyticsFilters): Observable<RankedDataResponse<PeakHourEntry>> {
+		return this.http.get<RankedDataResponse<PeakHourEntry>>(`${this.baseUrl}/peak-hours`, {
+			params: this.createParams(filters),
+		});
+	}
+
+	getStorePerformance(filters: AnalyticsFilters): Observable<RankedDataResponse<StoreRankEntry>> {
+		return this.http.get<RankedDataResponse<StoreRankEntry>>(`${this.baseUrl}/top-stores`, {
+			params: this.createParams(filters),
+		});
+	}
 }
